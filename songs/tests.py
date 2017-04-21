e8c1537ec98702fcb8ff8869b386364fc5702b1d
Brent Smith <bdchan305@gmail.com>
2017-04-21 18:20:01 +0000

    moved Client() into test method, added assert and lookup for obj.th

--- songs/tests.py
+++ songs/tests.py
@@ -5,18 +5,14 @@ from bs4 import BeautifulSoup
 # Create your tests here.
 
 class SimpleSiteTests(TestCase):
-    
-    c = Client()
-    
-    def setUp(self):
-        c = Client(HTTP_USER_AGENT='Mozilla/5.0')
-    
+
     def test_assertion(self):
         self.assertTrue(True)
 
     def test_index_page(self):
+        c = Client()
+        c = Client(HTTP_USER_AGENT='Mozilla/5.0')
         response = c.get('/')
         self.assertEqual(response.status_code, 200)
         obj = BeautifulSoup(response.content, "html.parser")
-
-        
+        self.assertEqual(obj.th, "Learning the Services")
