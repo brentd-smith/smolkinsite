f43316407ccea6f771eb836047ae111741ef38c9
Brent Smith <bdchan305@gmail.com>
2017-04-21 18:09:24 +0000

    added django.test.Client to test, added a test_index_page test

--- songs/tests.py
+++ songs/tests.py
@@ -1,8 +1,17 @@
 from django.test import TestCase
+from django.test import Client
 
 # Create your tests here.
 
 class SimpleSiteTests(TestCase):
     
+    def setUp(self):
+        c = Client(HTTP_USER_AGENT='Mozilla/5.0')
+    
     def test_assertion(self):
         self.assertTrue(True)
+
+    def test_index_page(self):
+        response = c.get('/')
+        self.assertEqual(response.status_code, 200)
+        
