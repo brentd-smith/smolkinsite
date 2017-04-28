f8e7559f5828e5ae424922ce395c44a1dc442312
Brent Smith <bdchan305@gmail.com>
2017-04-28 16:00:28 +0000

    updated the book list template title, added more tests

--- songs/tests.py
+++ songs/tests.py
@@ -45,16 +45,42 @@ class SimpleSiteTests(TestCase):
             # self.assertTrue(searchPattern in services, "Did not find {} in {}".format(searchPattern, services))
             self.assertTrue(re.search(s, str(obj)))
 
+
 class Text2TorahTests(TestCase):
     
+    # TODO: Create a loadTestData procedure that loads a limited set of data for testing purposes
+    # that will help keep the tests fast...
     def setUp(self):
-        text2torah.addBooksAndParshas()
-        
+        text2torah.text2db()
+
     def test_get_object_key(self):
         # filename: 3rd Triennial Noach 6th Aliyah.pdf
         # returns:  07 - Torah Readings/01 - Breshit (Genesis)/02 Parshat Noach/3rd Triennial Noach 6th Aliyah/
         result = text2torah.get_object_key("3rd Triennial Noach 6th Aliyah.pdf", debug=False)
         self.assertEqual(result, "07 - Torah Readings/01 - Breshit (Genesis)/02 Parshat Noach/3rd Triennial Noach 6th Aliyah")
+
+    def test_book_list(self):
+        c = Client(HTTP_USER_AGENT='Mozilla/5.0')
+        r = c.get('/TorahReading/')
+        self.assertEqual(r.status_code, 200)
+        
+        obj = BeautifulSoup(r.content, "html.parser")
+        title = obj.th
+        self.assertEqual(str(title), "<th>Books In The Torah</th>")
+        
+    def test_parsha_list(self):
+        c = Client(HTTP_USER_AGENT='Mozilla/5.0')
+        r = c.get('/TorahReading/Breshit/')
+        self.assertEqual(r.status_code, 200)
+        
+        obj = BeautifulSoup(r.content, "html.parser")
+        title = obj.th
+        self.assertEqual(str(title), "<th>Parshas in Breshit (Genesis)</th>")
+
+        # should have a list of Parshas, starting with Breshit    
+        first = obj.find("td")
+        self.assertTrue(re.search("Breshit", str(first)))
+        
 
 # Management Command Tests
 class ManagementCommandTests(TestCase):
